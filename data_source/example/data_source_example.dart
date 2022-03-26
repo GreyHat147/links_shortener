f03ab21f6c76ff0962adffd5f9b426ec877115f7
Carlos Rojas <carlosandresrojasvalbuena521@gmail.com>
2022-03-26 15:52:36 -0500

    data_source package created

---
+++ data_source/example/data_source_example.dart
@@ -0,0 +1,6 @@
+import 'package:data_source/data_source.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ data_source/lib/data_source.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library data_source;
+
+export 'src/data_source_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
