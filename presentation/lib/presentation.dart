51c77a0baa1a50e1bd6b0304574e56c43ed028c7
Carlos Rojas <carlosandresrojasvalbuena521@gmail.com>
2022-03-26 18:50:30 -0500

    presentation package created

---
+++ presentation/lib/presentation.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library presentation;
+
+export 'src/presentation_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
